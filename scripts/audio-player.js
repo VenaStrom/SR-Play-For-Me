2735fb113a0c65ae9e6c49ba9b7040ef8ea72a36
viggoStrom <viggostrom05@gmail.com>
2024-12-04 17:03:23 +0100

    progress bar

--- scripts/audio-player.js
+++ scripts/audio-player.js
@@ -3,8 +3,10 @@ class AudioPlayer {
     constructor() {
         this.DOM = document.querySelector("#player");
         this.playButton = this.DOM.querySelector("button");
-        this.timeDisplay = this.DOM.querySelector("#time");
+        this.timeDisplay = this.DOM.querySelector(".time-display");
+        this.progressBarContainer = this.DOM.querySelector(".progress-bar");
 
+        // Whether the current audio track is playing or not
         this.playing = false;
 
         this.playButtonInit();
@@ -20,5 +22,16 @@ class AudioPlayer {
                 this.playButton.querySelector("img").src = "assets/icons/icons8-play-48.png";
             }
         });
+    }
+
+    setProgress(fraction) {
+        const lowestBound = 0.03; // To prevent the progress bar from being too small
+        const highestBound = 1;
+
+        const cleanedFraction = parseFloat(fraction.toString().replace(/[^0-9.]/g, "")).toFixed(2);
+        const displayPercent = (cleanedFraction * (highestBound - lowestBound) + lowestBound) * 100;
+
+        const progressBar = this.progressBarContainer.querySelector(".foreground");
+        progressBar.style.width = displayPercent + "%";
     }
 }
